67a1a97eab859df9dd4dddb22c74b5f1acdde706
Darkmask101 <76608192+Darkmask101@users.noreply.github.com>
2024-04-20 12:48:36 +0530

    v.1.0.1
    added everything into a class created the login and the spam methods

--- sel.py
+++ sel.py
@@ -6,37 +6,57 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+class insta:
+    #SAGAR IF YOU ARE READING THIS INSTALL SELENIUM AND CHECK YOUR CHROME VERSION AND DOWNLOAD THE RESPECTIVE WEBDRIVER FOR YOUR CHROME 
+    service = Service(executable_path="/home/sourav/Desktop/chromedriver")
+    driver = webdriver.Chrome(service=service)
 
-#SAGAR IF YOU ARE READING THIS INSTALL SELENIUM AND CHECK YOUR CHROME VERSION AND DOWNLOAD THE RESPECTIVE WEBDRIVER FOR YOUR CHROME 
-service = Service(executable_path="/home/sourav/Desktop/chromedriver")
-driver = webdriver.Chrome(service=service)
+    #LOGIN CREDENTIALS
+    USERNAME = 'tapanbiswas2341@gmail.com'
+    PASS = 'XjuY#f9,7p;H)8Q'
 
-#LOGIN CREDENTIALS
-USERNAME = 'tapanbiswas2341@gmail.com'
-PASS = 'XjuY#f9,7p;H)8Q'
+    def loogin(self, USERNAME, PASS,driver,service):
+        driver.get('https://www.instagram.com/accounts/login/')
 
+        username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+        username_field.send_keys(USERNAME)
 
-driver.get('https://www.instagram.com/accounts/login/')
+        time.sleep(3)
 
-username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+        pass_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+        pass_field.send_keys(PASS)
 
-username_field.send_keys(USERNAME)
+        time.sleep(2)
 
-time.sleep(3)
+        login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')))
+        login_button.click()
 
-pass_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
-pass_field.send_keys(PASS)
-
-time.sleep(2)
-
-login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div')))
-time.sleep(1)
-login_button.click()
-
-time.sleep(30)
+        time.sleep(15)
+        driver.get('https://www.instagram.com/direct/t/17843018411832095')
+        time.sleep(20)
 
 
+    def spam(self,driver,service):
+        i = 1
+        skip_but = driver.find_element(By.XPATH, 'html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+        skip_but.click()
+        while i > 6:
+           #:
+            time.sleep(4)
+            text_but.send_keys(Keys.ENTER)
+            time.sleep(5)
+            text_but = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mount_0_0_Y5"]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[1]/p')))
+            text_but.send_keys("hello" + Keys.ENTER)
+            i += 1
+           #except e as error:
+               #print('error')
+
+
+# create an instance of the insta class
+my_insta = insta()
+#html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]
+# call the loogin method on the instance
+my_insta.loogin(my_insta.USERNAME, my_insta.PASS, my_insta.driver, my_insta.service)
 time.sleep(15)
-
-driver.get('https://www.instagram.com/agastya_shah.01/')
-time.sleep(9)
+my_insta.spam(my_insta.driver, my_insta.service)
+time.sleep(3)
